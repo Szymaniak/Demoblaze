74aad425dc31ddea0dae61ffd1951ce73e4da850
Szymaniak <szymanski.damian.1999@gmail.com>
2025-02-16 13:34:33 +0100

    modified test_data.py

--- test_data/test_data.py
+++ test_data/test_data.py
@@ -6,10 +6,10 @@ class DataReader:
     @staticmethod #metoda statyczna jest jedna, nie będzie tworzyło kopii.wielu obiektów
     def get_csv_data(filename):
         rows = []
-        data_file = open(filename, "r")
-        reader = csv.reader(data_file)
-        next(reader,None) #pomin pierwszy wiersz
-        for row in reader:
-            rows.append(row)
+        with open(filename, "r") as data_file:
+            reader = csv.reader(data_file)
+            next(reader,None) #pomin pierwszy wiersz
+            for row in reader:
+                rows.append(row)
 
         return rows
